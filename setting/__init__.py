f787a72ee21b6c3cc65f13c70bb6b452c3d300e0
cjhgo <1728533217en@gmail.com>
2016-09-18 01:21:38 +0800

    update

--- setting/__init__.py
+++ setting/__init__.py
@@ -1,6 +1,21 @@
 #coding:utf-8
 #created by chen @2016/9/17 17:29
 
+import os
 
 DEBUG = True
 AUTORELOAD = True
+
+MongoDB = {
+    "default":{
+        "host": "127.0.0.1"
+    }
+}
+
+API_ENV = os.getenv("API_ENV","local")
+
+if API_ENV == "local":
+    from local import *
+elif API_ENV == "production":
+    from production import *
+
