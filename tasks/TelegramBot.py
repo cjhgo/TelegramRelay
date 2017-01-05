3ab121d52cfa014bd2f739f99456aae5d69f777a
chenjunhang <1728533217en@gmail.com>
2017-01-05 15:44:22 +0800

    use message id as message id to support edited message

--- tasks/TelegramBot.py
+++ tasks/TelegramBot.py
@@ -24,68 +24,106 @@ db = client.queue
 
 
 @gen.coroutine
-def handle_keyword(body_list, update_id):
+def handle_keyword(body_list, message_id):
     body = [item.split('\n') for item in body_list.split('\n\n')]
-    yield db.research.insert({
-        "type": "keyword",
-        "message_id": update_id,
-        "content": body[0],
-        "urls": body[1] if len(body) > 1 else [],
-        "crts": datetime.datetime.utcnow()
-    })
+    yield db.research.update(
+        {
+            "message_id": message_id
+        },
+        {
+            "$set":
+            {
+                "type": "keyword",
+                "message_id": message_id,
+                "content": body[0],
+                "urls": body[1] if len(body) > 1 else [],
+                "crts": datetime.datetime.utcnow()
+            }
+
+        },
+        upsert=True)
 
 
 @gen.coroutine
-def handle_toreadlink(body_list, update_id):
+def handle_toreadlink(body_list, message_id):
     text = body_list.split('\n')
     for url in text:
-        yield db.toreadlink.insert({
-            "message_id": update_id,
-            "type": "url",
-            "link": url,
-            "crts": datetime.datetime.utcnow()
-        })
+        yield db.toreadlink.update(
+            {
+                "message_id": message_id
+            },
+            {
+                "$set":
+                {
+                    "message_id": message_id,
+                    "type": "url",
+                    "link": url,
+                    "crts": datetime.datetime.utcnow()
+                }
+            },
+            upsert=True)
 
 
 @gen.coroutine
-def handle_note(body_list, update_id):
-    yield db.notes.insert(
-        {
-            "message_id": update_id,
-            "type": "note",
-            "content": "\n".join(body_list),
-            "crts": datetime.datetime.utcnow()
-        }
-    )
+def handle_note(body_list, message_id):
+    yield db.notes.update(
+            {
+                "message_id": message_id
+            },
+            {
+                "$set":
+                {
+                    "message_id": message_id,
+                    "type": "note",
+                    "content": "\n".join(body_list),
+                    "crts": datetime.datetime.utcnow()
+                }
+            },
+            upsert=True)
 
 
 @gen.coroutine
-def handle_todo(body_list, update_id):
+def handle_todo(body_list, message_id):
     for _ in body_list:
-        yield db.todo.insert({
-            "message_id": update_id,
-            "type": "todo",
-            "task": _,
-            "crts": datetime.datetime.utcnow()
-        })
+        yield db.todo.update(
+            {
+                "message_id": message_id
+            },
+            {
+                "$set":
+                    {
+                        "message_id": message_id,
+                        "type": "todo",
+                        "task": _,
+                        "crts": datetime.datetime.utcnow()
+                    }
+            },
+            upsert=True)
 
 
 @gen.coroutine
-def handle_blog(body_list, update_id):
+def handle_blog(body_list, message_id):
     items = body_list.split('\n')
     url = items.pop(0)
     meta = {}
     for _ in items:
         key, value = _.split(': ', 1)
         meta[key] = value
-    yield db.blog.insert({
-        "message_id": update_id,
-        "url": url,
-        "source": meta.get("source"),
-        "about": meta.get("about"),
-        "description": meta.get("description"),
-        "crts": datetime.datetime.utcnow(),
-    })
+    yield db.blog.update(
+        {
+            "message_id": message_id
+        },
+        {
+            "$set":
+                {
+                    "url": url,
+                    "source": meta.get("source"),
+                    "about": meta.get("about"),
+                    "description": meta.get("description"),
+                    "crts": datetime.datetime.utcnow(),
+                }
+        },
+        upsert=True)
 
 message_handler = {
     "keyword": handle_keyword,
@@ -95,38 +133,41 @@ message_handler = {
     "blog": handle_blog,
 }
 
+
 @gen.coroutine
 def handle_message(messages, update_id):
     logging.debug("receive message %s", messages)
     data = messages["result"]
     if len(data) > 0:
-        update_id = data[-1].get("update_id")
         for message in data:
+            update_id = message.pop("update_id")
             flag = yield db.TelegramMessage.find_one({"update_id": update_id})
             if flag:
                 logging.debug("duplicate message occurs, %s", update_id)
                 break
+
             message = message.get("message", message.get("edited_message"))
+            message_id = message.pop("message_id")
             yield db.TelegramMessage.update(
                 {
-                    "message_id": message["message_id"]
+                    "message_id": message_id
                 },
                 {
                     "update_id": update_id,
-                    "message_id": message["message_id"],
+                    "message_id": message_id,
                     "crts": datetime.datetime.utcnow(),
                     "content": message
                 },
                 upsert=True)
             content = message.get("text", '').split("\n\n\n\n")  # content:all message
-            links = filter(lambda x: x[:4] == 'http', message["message"].get("text", '').split("\n"))
-            print links
+            links = filter(lambda x: x[:4] == 'http', message.get("text", '').split("\n"))
+            print '\n'.join(links)
             for texts in content:   # texts: the same kind of message
                 texts = texts.splitlines(True)
                 message_type = texts.pop(0)[:-1]
                 for text in ''.join(texts).split('\n\n\n'):  # text: one message
                     try:
-                        yield message_handler[message_type](text, update_id)  #
+                        yield message_handler[message_type](text, message_id)  #
                     except KeyError as e:
                         logging.debug("unsupported message type occurs %s", e)
 
--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 
 from handlers import message
+from handlers import vps
 
 urls = (
     (r"/api/telegram/vocabulary$", message.MessageRequestHandler),
-    (r"/v1/telegram/getUpdate$", message.MessageRequestHandler)
+    (r"/v1/telegram/getUpdate$", message.MessageRequestHandler),
+    (r"/v1/open/", vps.VpsRequestHandler)
 )
